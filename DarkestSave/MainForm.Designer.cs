f72ecb609d998f6e94e1e8ea438079f701ca7bda
wang225407 <86836519+wang225407@users.noreply.github.com>
2024-03-14 21:52:32 +0800

    first
    上传

---
+++ DarkestSave/MainForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace DarkestSave
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.txtSavePath = new System.Windows.Forms.TextBox();
+            this.btnSelectSavePath = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridSaveFile = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cbSaveLocation = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.reName = new System.Windows.Forms.ToolStripMenuItem();
+            this.readSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CreateTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FilePath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridSaveFile)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtSavePath
+            // 
+            this.txtSavePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSavePath.Location = new System.Drawing.Point(12, 12);
+            this.txtSavePath.Name = "txtSavePath";
+            this.txtSavePath.ReadOnly = true;
+            this.txtSavePath.Size = new System.Drawing.Size(328, 21);
+            this.txtSavePath.TabIndex = 0;
+            // 
+            // btnSelectSavePath
+            // 
+            this.btnSelectSavePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectSavePath.Location = new System.Drawing.Point(346, 12);
+            this.btnSelectSavePath.Name = "btnSelectSavePath";
+            this.btnSelectSavePath.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectSavePath.TabIndex = 1;
+            this.btnSelectSavePath.Text = "选择目录";
+            this.btnSelectSavePath.UseVisualStyleBackColor = true;
+            this.btnSelectSavePath.Click += new System.EventHandler(this.btnSelectSavePath_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.dataGridSaveFile);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(409, 319);
+            this.panel1.TabIndex = 2;
+            // 
+            // dataGridSaveFile
+            // 
+            this.dataGridSaveFile.AllowUserToAddRows = false;
+            this.dataGridSaveFile.AllowUserToResizeColumns = false;
+            this.dataGridSaveFile.AllowUserToResizeRows = false;
+            this.dataGridSaveFile.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridSaveFile.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridSaveFile.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FileName,
+            this.CreateTime,
+            this.FilePath});
+            this.dataGridSaveFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridSaveFile.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dataGridSaveFile.Location = new System.Drawing.Point(0, 41);
+            this.dataGridSaveFile.Name = "dataGridSaveFile";
+            this.dataGridSaveFile.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
+            this.dataGridSaveFile.RowTemplate.Height = 23;
+            this.dataGridSaveFile.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridSaveFile.Size = new System.Drawing.Size(409, 278);
+            this.dataGridSaveFile.TabIndex = 3;
+            this.dataGridSaveFile.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dataGridSaveFile_CellBeginEdit);
+            this.dataGridSaveFile.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridSaveFile_CellEndEdit);
+            this.dataGridSaveFile.CellMouseUp += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridSaveFile_CellMouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Controls.Add(this.cbSaveLocation);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(409, 41);
+            this.panel2.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(321, 9);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "添加存档";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // cbSaveLocation
+            // 
+            this.cbSaveLocation.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSaveLocation.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cbSaveLocation.FormattingEnabled = true;
+            this.cbSaveLocation.ItemHeight = 16;
+            this.cbSaveLocation.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cbSaveLocation.Location = new System.Drawing.Point(73, 8);
+            this.cbSaveLocation.Name = "cbSaveLocation";
+            this.cbSaveLocation.Size = new System.Drawing.Size(75, 24);
+            this.cbSaveLocation.TabIndex = 7;
+            this.cbSaveLocation.SelectedIndexChanged += new System.EventHandler(this.cbSaveLocation_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "存档位：";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reName,
+            this.readSave,
+            this.deleteSave});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(125, 70);
+            // 
+            // reName
+            // 
+            this.reName.Name = "reName";
+            this.reName.Size = new System.Drawing.Size(124, 22);
+            this.reName.Text = "重命名";
+            this.reName.Click += new System.EventHandler(this.reName_Click);
+            // 
+            // readSave
+            // 
+            this.readSave.Name = "readSave";
+            this.readSave.Size = new System.Drawing.Size(124, 22);
+            this.readSave.Text = "读取存档";
+            this.readSave.Click += new System.EventHandler(this.readSave_Click);
+            // 
+            // deleteSave
+            // 
+            this.deleteSave.Name = "deleteSave";
+            this.deleteSave.Size = new System.Drawing.Size(124, 22);
+            this.deleteSave.Text = "删除存档";
+            this.deleteSave.Click += new System.EventHandler(this.deleteSave_Click);
+            // 
+            // FileName
+            // 
+            this.FileName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.FileName.DataPropertyName = "FileName";
+            this.FileName.FillWeight = 66F;
+            this.FileName.HeaderText = "存档名";
+            this.FileName.Name = "FileName";
+            this.FileName.ReadOnly = true;
+            // 
+            // CreateTime
+            // 
+            this.CreateTime.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.CreateTime.DataPropertyName = "CreateTime";
+            this.CreateTime.FillWeight = 34F;
+            this.CreateTime.HeaderText = "创建时间";
+            this.CreateTime.Name = "CreateTime";
+            this.CreateTime.ReadOnly = true;
+            // 
+            // FilePath
+            // 
+            this.FilePath.DataPropertyName = "FilePath";
+            this.FilePath.HeaderText = "文件路径";
+            this.FilePath.Name = "FilePath";
+            this.FilePath.ReadOnly = true;
+            this.FilePath.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 370);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnSelectSavePath);
+            this.Controls.Add(this.txtSavePath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "暗黑地牢存档工具";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridSaveFile)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSavePath;
+        private System.Windows.Forms.Button btnSelectSavePath;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridSaveFile;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem readSave;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ComboBox cbSaveLocation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem deleteSave;
+        private System.Windows.Forms.ToolStripMenuItem reName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CreateTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FilePath;
+    }
+}
+
---
+++ DarkestSave/MainForm.cs
@@ -0,0 +1,219 @@
+﻿using DarkestSave.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DarkestSave
+{
+    public partial class MainForm : Form
+    {
+        Config config = new Config();
+        string configFilePath = @"Configs/Config.txt";
+        DataGridViewRow setectedRow;
+        string oldFileName;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if(File.Exists(configFilePath) )
+            {
+                config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(configFilePath));
+            }
+            if(config != null && !string.IsNullOrEmpty(config.SavePath))
+            {
+                this.txtSavePath.Text = config.SavePath;
+                LoadSave();
+            }
+        }
+
+        private void btnSelectSavePath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog ofd = new FolderBrowserDialog();
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                this.txtSavePath.Text = ofd.SelectedPath;
+                if(config == null)
+                {
+                    config = new Config();
+                }
+                this.config.SavePath = ofd.SelectedPath;
+                File.WriteAllText(configFilePath, JsonConvert.SerializeObject(config));
+                LoadSave();
+            }
+        }
+
+        private void LoadSave()
+        {
+            this.cbSaveLocation.SelectedIndex = 0;
+            RefreshSaveLocation();
+        }
+
+        private void cbSaveLocation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //this.dataGridSaveFile.DataSource = null;
+            //RefreshSaveLocation();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            int loca = this.cbSaveLocation.SelectedIndex + 1;
+            var path = Path.Combine(config.SavePath, "Save");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string direPath = Path.Combine(config.SavePath, $"profile_{this.cbSaveLocation.SelectedIndex}");
+            string zipFile = Path.Combine(path, $"{loca}_{DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss")}.zip");
+            ZipFile.CreateFromDirectory(direPath, zipFile);
+            RefreshSaveLocation();
+        }
+
+        private void RefreshSaveLocation()
+        {
+            int loca = this.cbSaveLocation.SelectedIndex + 1;
+            var path = Path.Combine(config.SavePath, "Save");
+            if (Directory.Exists(path))
+            {
+                var files = Directory.GetFiles(path);
+                var zipFiles = files.Where(f => f.EndsWith(".zip")).ToList();
+                List<ZipFileModel> zipModels = new List<ZipFileModel>();
+                foreach (var zipFile in zipFiles)
+                {
+                    FileInfo file = new FileInfo(zipFile);
+                    ZipFileModel zipModel = new ZipFileModel(Path.GetFileNameWithoutExtension(file.Name),
+                        file.FullName,file.CreationTime);
+                    zipModels.Add(zipModel);
+                }
+                zipModels.Sort((z, a) =>
+                {
+                    return (int)(a.CreateTime.Ticks/1000 - z.CreateTime.Ticks/1000);
+                });
+                this.dataGridSaveFile.DataSource = zipModels;
+            }
+        }
+
+        private void dataGridSaveFile_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (e.RowIndex >= 0)
+                {
+                    dataGridSaveFile.ClearSelection();
+                    dataGridSaveFile.Rows[e.RowIndex].Selected = true;
+                    contextMenuStrip1.Show(MousePosition.X, MousePosition.Y);
+                }
+            }
+        }
+
+        private void reName_Click(object sender, EventArgs e)
+        {
+            var cell = dataGridSaveFile.SelectedRows[0].Cells["FileName"];
+            cell.ReadOnly = false;
+        }
+
+        private void readSave_Click(object sender, EventArgs e)
+        {
+            var model = dataGridSaveFile.SelectedRows[0].DataBoundItem as ZipFileModel;
+            if (MessageBox.Show($"确定要读取存档{model.FileName}到存档位{cbSaveLocation.Text}吗？", "提示",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                UncompressZip(model.FilePath, Path.Combine(config.SavePath, $"profile_{this.cbSaveLocation.SelectedIndex}"));
+                MessageBox.Show("已完成");
+            }
+        }
+
+        private void deleteSave_Click(object sender, EventArgs e)
+        {
+            var model = dataGridSaveFile.SelectedRows[0].DataBoundItem as ZipFileModel;
+            if (MessageBox.Show($"确定要删除{model.FileName}吗？", "提示",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                File.Delete(model.FilePath);
+                RefreshSaveLocation();
+            }
+        }
+
+        private void dataGridSaveFile_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            var fileModel = dataGridSaveFile.Rows[e.RowIndex].DataBoundItem as ZipFileModel;
+            oldFileName = fileModel.FileName;
+        }
+
+        private void dataGridSaveFile_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            var fileModel = dataGridSaveFile.Rows[e.RowIndex].DataBoundItem as ZipFileModel;
+            if(!string.IsNullOrEmpty(oldFileName) && oldFileName != fileModel.FileName)
+            {
+                var files = Directory.GetFiles(Path.GetDirectoryName(fileModel.FilePath));
+                var fileNames = files.Select(f => Path.GetFileNameWithoutExtension(f)).ToList();
+                string fileName = fileModel.FileName;
+                if (fileNames.Any(f => f == fileModel.FileName))
+                {
+                    MessageBox.Show("文件名已存在");
+                    fileModel.FileName = oldFileName;
+                }
+                else
+                {
+                    string newFilePath = Path.Combine(Path.GetDirectoryName(fileModel.FilePath), fileModel.FileName + ".zip");
+                    File.Move(fileModel.FilePath, newFilePath);
+                    fileModel.FilePath = newFilePath;
+                }
+            }
+            dataGridSaveFile.Rows[e.RowIndex].Cells[e.ColumnIndex].ReadOnly = true;
+            oldFileName = null;
+        }
+
+        private void UncompressZip(string zipFile, string outPath)
+        {
+            using (FileStream fs = new FileStream(zipFile, FileMode.Open))
+            {
+                using (ZipArchive zr = new ZipArchive(fs))
+                {
+                    foreach (var en in zr.Entries)
+                    {
+                        FileInfo path = new FileInfo(Path.Combine(outPath, en.FullName));
+                        if (!path.Directory.Exists)
+                        {
+                            path.Directory.Create();
+                        }
+                        if (en.Name == "")
+                        {
+                            Directory.CreateDirectory(path.FullName);
+                        }
+                        else
+                        {
+                            en.ExtractToFile(path.FullName, true);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    class ZipFileModel
+    {
+        public ZipFileModel(string name, string path, DateTime dateTime)
+        {
+            this.FileName = name;
+            this.FilePath = path;
+            this.CreateTime = dateTime;
+        }
+
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
